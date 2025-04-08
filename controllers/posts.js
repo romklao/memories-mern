8a4c394e1c40c86178986862c649e17e9c43d3ab
Romklao Chainuwong <rk120879@gmail.com>
2025-04-08 16:27:38 -0700

    make it better

---
+++ controllers/posts.js
@@ -0,0 +1,71 @@
+import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: `No post with id: ${id}` });
+  }
+
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(
+      id,
+      { creator, title, message, tags, selectedFile },
+      { new: true }
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: `Post not found` });
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post deleted successfully." });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, {
+    likeCount: post.likeCount + 1,
+  });
+  res.json(updatedPost);
+};
