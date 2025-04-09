b32826d7a4dc436e98dba4920a80045a804148af
Romklao Chainuwong <rk120879@gmail.com>
2025-04-08 20:23:22 -0700

    Update package

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
